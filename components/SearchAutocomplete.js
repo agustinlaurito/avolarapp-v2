f92cdf93921e991a721d2dfa528d9cad541278ac
Agustin <agustinlaurito@hotmail.com>
2024-03-01 01:05:56 -0300

    [FIX] Search Bar

--- components/SearchAutocomplete.js
+++ components/SearchAutocomplete.js
@@ -1,6 +1,6 @@
 'use client';
 
-import { Flex, FormControl, FormHelperText, FormLabel } from "@chakra-ui/react";
+import { Flex, FormControl, FormHelperText, FormLabel, Icon, IconButton, InputGroup, InputRightElement, useToast } from "@chakra-ui/react";
 import {
 	AutoComplete,
 	AutoCompleteInput,
@@ -13,15 +13,38 @@ import { useRouter } from "next/navigation";
 import { useEffect, useState } from "react";
 import { getAutocompleteList } from "@/utils/api";
 import { useAirportContext } from "@/app/context/airport";
+import { BiSearch } from "react-icons/bi";
 
 function SearchAutocomplete() {
 
 	const router = useRouter();
+	const toast = useToast();
 	const { setAirport } = useAirportContext();
 
 	const [value, setValue] = useState('');
 
 	const [list, setList] = useState(AIRPORTS);
+
+	const onSearch = () => {
+
+		const airportExists = list.find(entry => entry.localCode === value || entry.iataCode === value || entry.oaciCode === value);
+
+		if(airportExists) {
+
+			router.push(`/airport/${airportExists.localCode}`);
+		}
+		else {
+
+			toast({
+				title: 'Aeropuerto no encontrado.',
+				status: 'error',
+				duration: 2000,
+				isClosable: true,
+			});
+		}
+
+
+	};
 
 	useEffect(() => {
 
@@ -29,25 +52,20 @@ function SearchAutocomplete() {
 
 	}, []);
 
-
-
-	// useEffect(() => {
-	// 	document.addEventListener('keydown', function(event){
-	// 		if(event.key === 'Enter') {
-	// 			router.push(`/airport/${value}`);
-	// 			setAirport(null);
-	// 		}
-	// 	});
-	// }, []);
-
 	return (
 		<FormControl size="lg">
-			<AutoComplete  openOnFocus suggestWhenEmpty={false}>
-				<AutoCompleteInput value={value} onChange={e => setValue(e.target.value)} bg="white" variant='flushed' borderRadius="xl" shadow='table' fontSize={{ base: 'xs', md: 'md' }} placeholder="Ingresa un aeropuerto ..." pl={{ base: 2, md: 5 }} />
+			<AutoComplete suggestWhenEmpty={false} listAllValuesOnFocus={false}>
+				<InputGroup size={'lg'}>
+					<AutoCompleteInput _focus={{ boxShadow: 'var(--chakra-shadows-table);' }} value={value} onChange={e => setValue(e.target.value)} bg="white" variant='flushed' borderRadius="xl" shadow='table' fontSize={{ base: 'xs', md: 'md' }} placeholder="Ingresa un aeropuerto ..." pl={{ base: 2, md: 5 }} />
+
+					<InputRightElement>
+						<IconButton colorScheme={'brand.info'} roundedLeft={'0'} onClick={onSearch} size={'lg'} icon={<Icon as={BiSearch} />} />
+					</InputRightElement>
+				</InputGroup>
 				<AutoCompleteList w='full'>
 					{ list.map((airport, cid) => (
 						<AutoCompleteItem
-							onClick={() => { setValue(airport.localCode); router.push(`/airport/${airport.localCode}`); setAirport(null)}}
+							onClick={() => { setValue(airport.oaciCode || airport.iataCode || airport.localCode) }}
 							key={`option-${cid}`}
 							value={airport.description}
 							textTransform="capitalize"
